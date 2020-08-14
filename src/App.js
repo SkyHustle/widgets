4c32c594698ab7b1e2255b2d523afc808d9a50d6
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-14 13:57:24 -1000

    Create Accordion component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Accordion from './components/Accordion';
 
 export default () => {
-    return <h1>Widgets App</h1>;
+    return (
+        <div>
+            <h1>Widgets App</h1>
+            <Accordion />
+        </div>
+        
+    )
 };
---
+++ src/components/Accordion.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Accordion = () => {
+    return <h3>Accordion</h3>;
+}
+
+export default Accordion;
