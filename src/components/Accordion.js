0cecdfd5978ca756b8d20e45c0eda8209bdfd755
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-14 17:59:03 -1000

    When title is clicked, show the index of that title

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
+    // Array Destructuring
+    const [activeIndex, setActiveIndex] = useState(null);
+    
+    const onTitleClick = (index) => {
+        setActiveIndex(index)
+    };
+
+    const renderedItems = items.map((item, index) => {
         return (
             <React.Fragment key={item.title}>
-                <div className="title active">
+                <div onClick={() => onTitleClick(index)} className="title active">
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
@@ -15,7 +22,12 @@ const Accordion = ({ items }) => {
         )
     });
 
-    return <div className="ui styled accordion">{renderedItems}</div>;
-}
+    return (
+        <div className="ui styled accordion">
+            {renderedItems}
+            <h3>{activeIndex}</h3> 
+        </div>
+    )
+};
 
 export default Accordion;
