ebc15b6e0212fa2a702ecd69b03a371e262e34a8
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-17 11:39:37 -1000

    Set default term, save api results to state

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,23 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('code');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
-                }
-            })
-        }
-
+                    srsearch: term,
+                },
+            });
+            setResults(data.query.search);
+        };
         search();
-        
     }, [term]);
 
     return (
