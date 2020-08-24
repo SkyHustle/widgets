04fcb04deb59ef59527338a75717a2ab79c26acb
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-24 12:31:01 -1000

    Detect that the url has changed

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Link = ({ href, text, className }) => {
     const onClick = (event) => {
         event.preventDefault();
-        window.history.pushState({}, '', href)
+        window.history.pushState({}, '', href);
+
+        // Route compnent listens for this event
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
     };
 
     return (
