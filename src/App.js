734aa2983238396ae2718d4beadef3a49411968f
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-24 10:08:28 -1000

    Complete hard coding navigation, render component to screen based on pathname in searchbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
-// import Search from './components/Search';
-// import DropDown from './components/DropDown';
-// import Translate from './components/Translate';
+import Search from './components/Search';
+import DropDown from './components/DropDown';
+import Translate from './components/Translate';
 
 const items = [
     {
@@ -19,32 +19,48 @@ const items = [
     }
 ]
 
-// const options = [
-//     {
-//         label: "The Color Red",
-//         value: "red"
-//     },
-//     {
-//         label: "The Color Green",
-//         value: "green"
-//     },
-//     {
-//         label: "The Color Blue",
-//         value: "blue"
-//     }
-// ]
-
-const renderComponent = () => {
-    const pathname = window.location.pathname;
-    switch (pathname) {
-        case "/":
-            return <Accordion items={items} />;
-        default:
-            console.log("Default");
+const options = [
+    {
+        label: "The Color Red",
+        value: "red"
+    },
+    {
+        label: "The Color Green",
+        value: "green"
+    },
+    {
+        label: "The Color Blue",
+        value: "blue"
     }
-}
+]
+
 
 export default () => {
+    const [selected, setSelected] = useState(options[0])
+
+    const renderComponent = () => {
+        const pathname = window.location.pathname;
+        switch (pathname) {
+            case "/":
+                return <Accordion items={items} />;
+            case "/list":
+                return <Search />;
+            case "/dropdown":
+                return (
+                    <DropDown 
+                        options={options}
+                        selected={selected}
+                        onSelectedChange={(e) => setSelected(e)}
+                        label={"Choose A Color"}
+                    />
+                );
+            case "/translate":
+                return <Translate />
+            default:
+                console.log("Default");
+        }
+    }
+
     return (
         <div>
             {renderComponent()}
